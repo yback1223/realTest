9ea0124e98f968ed88991ea7e77b61638af95bab
yback1223 <yoyob1223@gmail.com>
2023-03-27 16:21:10 +0900

    feat: hello

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,30 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'temp',
+        'style',
+        'refactor',
+        'test',
+        'chore',
+        'hotfix',
+        'docs',
+      ],
+    ],
+    'header-max-length': [2, 'always', 50],
+    'body-max-length': [0, 'never'],
+  },
+};
