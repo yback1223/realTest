90d88f26bbe2560f733d69d2780aef3c7f620674
yback1223 <yoyob1223@gmail.com>
2023-03-27 14:29:09 +0900

    feat: Add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'temp',
+        'style',
+        'refactor',
+        'test',
+        'chore',
+        'hotfix',
+        'docs',
+      ],
+    ],
+    'header-max-length': [2, 'always', 50],
+    'body-max-length': [0, 'never'],
+  },
+};
