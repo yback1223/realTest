88a8c5588e3695ba8435ec0bc33817b9bf43e6f5
yback1223 <yoyob1223@gmail.com>
2023-03-27 16:58:38 +0900

    feat: #2 Add commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'subject-case': [
-      2,
-      'never',
-      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
-    ],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'type-empty': [2, 'never'],
